8571d77a5e3647de8ed5057c41bff2453beb8e02
TuyenNguyen1302 <tuyenbb1302@gmail.com>
2022-03-19 20:39:23 +0700

    update

--- javascript/HomePage.js
+++ javascript/HomePage.js
@@ -114,3 +114,31 @@ popupListImg.forEach((imgE, popupIndex) => {
         updateImagePopup(popupIndex);
     })
 })
+
+// POPUP TAB
+function openTab(evt, tabName) {
+    var i, tabcontent, tablink;
+    tabcontent = document.getElementsByClassName('tabcontent');
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablink = document.getElementsByClassName('tablink');
+    for (i = 0; i < tabcontent.length; i++) {
+        tablink[i].className = tablink[i].className.replace(" active", "");
+    }
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+
+// HEADER SCROLL
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if(document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+        document.querySelector('.nav').style.cssText = `padding: 10px;`;
+        document.querySelector('.heading-logo').style.cssText = `width: 100px;`;
+    } else {
+        document.querySelector('.nav').style.cssText = `padding: 20px `;
+        document.querySelector('.heading-logo').style.cssText = `width: 130px;`;
+    }
+}
